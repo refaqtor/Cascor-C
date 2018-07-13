cb53f13ea944bf25122a6bc83dd01e9fe2da0446
Ian Chiu <ianjchiu@gmail.com>
2018-07-13 07:05:30 -0400

    Working dbg_malloc.c

--- src/toolkit/dbg_malloc.c
+++ src/toolkit/dbg_malloc.c
@@ -1,4 +1,5 @@
 #define DEBUG_ON 1
+#define PADDING 32
 #include "dbg_malloc.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,43 +7,47 @@
 #include <string.h>
 #include <signal.h>
 
+
 void *dbg_malloc(size_t size){
-    void *p = malloc(size+2000);
-    memset(p, 0xEE, size+2000);
+    void *p = malloc(size+2 * PADDING);
+    if(!p) return NULL;
+    memset(p, 0xEE, size+2 * PADDING);
     *(((size_t *) p) + 1) = size;
-    return p+1000;
+    return p+PADDING;
 }
 
 void *dbg_realloc(void *p, size_t new_size){
     void *actual_p, *new_p;
     size_t old_size;
-    actual_p = p - 1000;
+    actual_p = p - PADDING;
     old_size = *(((size_t*) actual_p) + 1);
-    new_p = realloc(actual_p, new_size);
+    new_p = realloc(actual_p, new_size + 2 * PADDING);
+    if(!new_p) return NULL;
     *(((size_t *)new_p) + 1) = new_size;
-    memset((new_p + old_size + 2000), 0xEE, new_size - old_size);
-    return new_p + 1000;
+    if(new_size > old_size){
+        memset((new_p + old_size + 2 * PADDING), 0xEE, new_size - old_size);
+    }
+    return new_p + PADDING;
 }
-
 void dbg_free(void *p){
     void *actual_p;
     size_t size;
     int i;
     if(!p) return;
-    actual_p = p - 1000;
-    *(((size_t *)p) + 1) = 0xEEEEEEEEEEEEEEEE;
-    for(i = 0; i < 1000; i++){
-        if( *(((char *)p) + i) != 0xEE){
-            printf("Corrupted on the first %d byte of pointer %p", i, p);
+    actual_p = p - PADDING;
+    size = *(((size_t *)actual_p) + 1);
+    *(((size_t *)actual_p) + 1) = 0xEEEEEEEEEEEEEEEE;
+    for(i = 0; i < PADDING; i++){
+        if( *(((char *)actual_p) + i) != ((char)0xEE)){
+            printf("Corrupted on the first %d byte of pointer %p, at beginning\n", i, p);
             raise(11);
         }
     }
-    for(i = 0; i < 1000; i++){
-        if( *(((char *)p) + i + size) != 0xEE){
-            printf("Corrupted on the first %d byte of pointer %p", i, p);
+    for(i = 0; i < PADDING; i++){
+        if( *(((char *)p) + i + size) != ((char)0xEE)){
+            printf("Corrupted on the first %d byte of pointer %p, at end\n", i, p);
             raise(11);
         }
     }
     free(actual_p);
 }
-
