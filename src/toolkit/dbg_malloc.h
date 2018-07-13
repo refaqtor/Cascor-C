d903159366e6430a8eb9b6f5fbf7cd1f0425f6fe
Ian Chiu <ianjchiu@gmail.com>
2018-07-13 01:40:45 -0400

    Adding a debugging malloc, realloc, and free to check for invalid read and writes

---
+++ src/toolkit/dbg_malloc.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * functionally works like malloc, realloc, and free,
+ * but adds overhead to the beginning and end of each datablock
+ * to check for memory corruption.
+ */
+
+
+void *dbg_malloc( size_t size );
+void *dbg_realloc( void * p, size_t new_size );
+void *dbg_free( void *p );
+
+#define malloc(X) dbg_malloc(X)
+#define realloc(X,Y) dbg_realloc(X,Y)
+#define free(X) dbg_free(X)
